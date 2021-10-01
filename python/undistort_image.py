ee05d7024915228fb1edd0ca6dba0e5a49b6953b
cedarwarman <cedardalewarman@gmail.com>
2021-09-30 22:13:14 -0700

    Add undistort script and cleanup.

---
+++ python/undistort_image.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2 as cv
+import glob
+import yaml
+import os
+
+# Load calibration values
+calibration_path = os.path.join(os.path.dirname(__file__),
+                                '../data/calibration.yaml')
+
+with open(calibration_path) as f:
+    loaded_dict = yaml.load(f, Loader=yaml.SafeLoader)
+    print("Calibration loaded")
+
+mtx = np.asarray(loaded_dict.get('camera_matrix'))
+dist = np.asarray(loaded_dict.get('dist_coeff'))
+
+# Undistorting the images
+images = glob.glob('*.jpg')
+
+for fname in images:
+    print("Undistorting: " + fname)
+    img = cv.imread(fname)
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+    # Undistort
+    dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+
+    # Crop the image
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+
+    # Write
+    output_name = str(os.path.splitext(fname)[0]) + '_undistorted.jpg'
+    cv.imwrite(output_name, dst)
